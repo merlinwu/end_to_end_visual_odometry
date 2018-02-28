e676516db0781a6817126d014bae3057da00aa01
Chunshang Li <jack.chunshang.li@gmail.com>
2018-02-28 13:16:38 -0500

    save progress

--- main.py
+++ main.py
@@ -18,8 +18,7 @@ pose_size = 13
 input_data = tf.placeholder(tf.float32, name="inputs",
                             shape=[batch_size, max_timesteps, input_width, input_height, input_channels])
 
-
-# output_data = tf.placeholder(tf.float32, name="outputs", shape=[batch_size, max_timesteps, pose_size])
+output_data = tf.placeholder(tf.float32, name="outputs", shape=[batch_size, max_timesteps, pose_size])
 
 
 # ================= Build Graph Model ===================
@@ -36,6 +35,17 @@ def fc_model(inputs):
     fc_128 = tf.contrib.layers.fully_connected(inputs, 128, scope="fc_128", activation_fn=tf.nn.relu)
     fc_12 = tf.contrib.layers.fully_connected(fc_128, 12, scope="fc_12", activation_fn=tf.nn.relu)
     return fc_12
+
+
+# pose 1 in xyz + rpy
+# pose 2 in xyz + quaternion
+# returns quaternion
+def pose_comp(pose_1, pose_2):
+    return tf.concat([pose_1, [0]], 0) + pose_2
+
+
+def calc_covar(input):
+    return input
 
 
 cnn_outputs = []
@@ -68,14 +78,16 @@ with tf.variable_scope("Fully-Connected"):
         fc_output = fc_model(fc_input)
         fc_outputs.append(fc_output)
 
-init_pose = tf.constant([0] * 6, dtype=tf.float32, name="initial_pose", shape=[pose_size])
-final_outputs = tf.get_variable(name="outputs", shape=[batch_size, max_timesteps, pose_size], dtype=tf.float32,
-                                initializer=tf.zeros_initializer)
-
+init_pose = tf.constant([0, 0, 0, 0, 0, 0, 1], dtype=tf.float32, name="initial_pose", shape=[pose_size])
+poses = [init_pose]
+covars = []
 with tf.variable_scope("SE3"):
     # for pose in fc_outputs:
-    for i in range(0, len(fc_outputs))
-        pose[:, 0:6] +=
+    for i in range(0 + 1, len(fc_outputs) + 1):
+        pose = pose_comp(poses[i - 1][0:6], fc_outputs[i][0:6])
+        covar = calc_covar(fc_outputs[3:6])
+        poses.append(pose)
+        covars.append(covar)
 
 sess = tf.Session()
 init = tf.global_variables_initializer()
